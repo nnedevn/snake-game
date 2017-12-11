9974327c1af6bc931ac1a8ff2aec2cd7601b06eb
Nick_edu <nick_edu@nameless.local>
2017-12-11 10:49:24 -0800

    Adds the canvas grid

--- js/main.js
+++ js/main.js
@@ -1,12 +1,78 @@
 $(function(){
 
 
+// 
+let canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
+let boxSize = 10;
+boxes = Math.floor(400/boxSize);
+
+ direction = 'RIGHT';
 
 
 
 
 
-console.log('in');
+
+//create game loop
+
+drawBoard();
+
+function drawBoard(){
+  context.beginPath();
+  context.fillStyle = 'white';
+  context.lineWidth = 1;
+  contextStrokeStyle = 'black';
+  for (let row = 0; row <= boxes; row+=1) {
+    for(let col = 0; col <=boxes; col +=1){
+      let x = col * boxSize;
+      let y = row * boxSize;
+      context.rect(x,y,boxSize, boxSize);
+      context.fill()
+      context.stroke();
+    }
+  }
+
+}
+
+function tick(){
+  // update every100ms
+
+  //determine the direction
+    // if left cordinates change to -1, 0 relative to the current position
+
+    //insert new position on the que
+    //remove the last position from the que
+
+}
+
+  
+//  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
