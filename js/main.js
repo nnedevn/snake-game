84edac667c808ebeb259c21cb711ee475ac61b0c
Nick Nedev <nedevn@gmail.com>
2017-12-12 21:07:04 -0800

    Wires the game object with the Snake object

--- js/main.js
+++ js/main.js
@@ -2,52 +2,37 @@ var SNAKE = {}
 
 SNAKE.game = (function() {
 
-  let framerate = 60;
+
+  SNAKE.width = 200;
+  SNAKE.height = 200;
+  let framerate = 1;
+  let snake;
 
   let canvas;
   let canvasContext;
-  let food;
-  let score;
-  let speed = 130;
-
-  let xPos = 0;
-  let yPos = 0;
 
 
   //Snake Array
   let snakeArray;
 
-  function createSnake() {
-    console.log('Creating snake');
-    let snakeLength = 5;
-    snakeArray = [];
-
-    for (let i = snakeArray.length - 1; i >= 0; i -= 1) {
-      snakeArray.push({
-        x: i,
-        y: 0
-      });
-      console.log(i);
-    }
-
-  }
-
-  function gameLoop() {
-    console.log('loop');
-    xPos += 2;
-    yPos += 4;
-    //canvasContext.clearRect(0, 0, 100, 100); //clear the canvas
-    canvasContext.fillStyle = '#F00';
-    canvasContext.fillRect(xPos, yPos, 30, 50); //a moving rect
-    // setInterval(gameLoop, 1000);
-    window.requestAnimationFrame(gameLoop);
+   function gameLoop() {
+  
+    canvasContext.clearRect(0, 0, SNAKE.width, SNAKE.height); //clear the canvas
+    snake.advance();
+    snake.drawSnake(canvasContext);
+    // window.requestAnimationFrame(gameLoop);
+    setTimeout(gameLoop, 1000/framerate);
   }
 
   // Initializing function
   function init() {
-    console.log('Initializing')
+    console.log('Initializing..')
     canvas = document.getElementById('canvas');
     canvasContext = canvas.getContext('2d');
+    canvas.setAttribute('width', SNAKE.width);
+    canvas.setAttribute('height', SNAKE.height);
+
+    snake = SNAKE.snake();
 
     gameLoop();
   }
@@ -58,41 +43,45 @@ SNAKE.game = (function() {
 
 }());
 
-SNAKE.snake = (function() {
+SNAKE.snake = function() {
   let posArr = [];
   let cellSize = 10;
 
 
-  posArr.push[6, 4];
-  posArr.push[5, 4]
-  posArr.push[4, 4];
+  posArr.push([6, 4]);
+  posArr.push([5, 4]);
+  posArr.push([4, 4]);
   let direction = 'right';
 
   function drawSection(canvasContext, pos) {
+    console.log(pos[1]);
     let x = SNAKE.snake.cellSize * pos[0];
     let y = SNAKE.snake.cellSize * pos[1];
-    canvasContext.fillRect(x, y, SNAKE.snake.cellSize, SNAKE.snake.cellSize);
+    canvasContext.fillRect(x, y, cellSize, cellSize);
   }
 
   function drawSnake(canvasContext){
-    //save the current state of the canvas - blanc
+    console.log('Drawing Snake');
+    // save the current state of the canvas - blanc
     canvasContext.save();
-    //changes
-    canvasContext.fillStyle = '#F00';
+    // changes
+     
+    canvasContext.fillStyle = 'black';
     for (let i = 0; i< posArr.length; i+=1){
-      drawSnake(canvasContext, posArr[i]);
+      // console.log('position', posArr[i]);
+      drawSection(canvasContext, posArr[i]);
     }
 
     //restore the state of the canvas before changes
     canvasContext.restore();
   }
-
+  //increment head position by one
   function advance(){
-
+    console.log(posArr);
     let nextPos = posArr[0].slice();
     nextPos[0] +=1;
 
-    //ass next position at the start
+    //next position at the start
     posArr.unshift(nextPos);
     //remove the last position
     posArr.pop();
@@ -106,7 +95,7 @@ SNAKE.snake = (function() {
 
 
 
-}());
+};
 
 
 
