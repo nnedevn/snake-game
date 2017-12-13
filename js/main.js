b1499cf84152707ccdd33b29c57d7d29564536ad
Nick Nedev <nedevn@gmail.com>
2017-12-12 22:36:16 -0800

    Adds a check for valid movement direction

--- js/main.js
+++ js/main.js
@@ -2,12 +2,10 @@ var SNAKE = {}
 
 SNAKE.game = (function() {
 
-
   SNAKE.width = 200;
   SNAKE.height = 200;
   let framerate = 10;
   let snake;
-
   let canvas;
   let canvasContext;
 
@@ -15,28 +13,37 @@ SNAKE.game = (function() {
   //Snake Array
   let snakeArray;
 
-  function addEventListeners(){
-    document.addEventListener('keydown', (evt)=>{
+  function addEventListeners() {
+    document.addEventListener('keydown', (evt) => {
+      evt.preventDefault();
       let directionKeys = {
         37: 'left',
         38: 'up',
-        39: 'down',
-        40: 'right'
+        39: 'right',
+        40: 'down'
       }
 
       let key = evt.which;
+      let direction = directionKeys[key];
+
+      //check if direction has been given
+      if (direction) {
+        snake.setDirection(direction);
+
+      }
+
 
 
     });
   }
- 
-   function gameLoop() {
-  
+
+  function gameLoop() {
+
     canvasContext.clearRect(0, 0, SNAKE.width, SNAKE.height); //clear the canvas
     snake.advance();
     snake.drawSnake(canvasContext);
     // window.requestAnimationFrame(gameLoop);
-    setTimeout(gameLoop, 1000/framerate);
+    setTimeout(gameLoop, 1000 / framerate);
   }
 
   // Initializing function
@@ -73,18 +80,17 @@ SNAKE.snake = function() {
     let x = cellSize * pos[0];
     let y = cellSize * pos[1];
     canvasContext.fillRect(x, y, cellSize, cellSize);
-    
+
   }
 
-  function drawSnake(canvasContext){
+  function drawSnake(canvasContext) {
 
-    // save the current state of the canvas - blanc
+    // save the current state of the canvas
     canvasContext.save();
     // changes
-     
+
     canvasContext.fillStyle = 'black';
-    for (let i = 0; i< posArr.length; i+=1){
-      // console.log('position', posArr[i]);
+    for (let i = 0; i < posArr.length; i += 1) {
       drawSection(canvasContext, posArr[i]);
     }
 
@@ -92,9 +98,9 @@ SNAKE.snake = function() {
     canvasContext.restore();
   }
   //increment head position by one
-  function advance(){
+  function advance() {
     let nextPos = posArr[0].slice();
-    nextPos[0] +=1;
+    nextPos[0] += 1;
 
     //next position at the start
     posArr.unshift(nextPos);
@@ -102,10 +108,34 @@ SNAKE.snake = function() {
     posArr.pop();
   }
 
+  function directionIsValid(inputDirection, movingDirection) {
+    console.log('movingDirection',movingDirection);
+    console.log('inputDirection',inputDirection); 
+    let possibleDirections = [];
+    if (movingDirection == 'up' || movingDirection == 'down') {
+
+      possibleDirection = ['left', 'right'];
+      console.log('possibleDirections',possibleDirections);
+    } else if (movingDirection === 'left' || movingDirection === 'right') {
+      possibleDirections = ['up', 'down'];
+      console.log(possibleDirections);
+    }
+
+    if (possibleDirections.includes(inputDirection)) return true;
+  }
+
+  function setDirection(inputDirection) {
+    if (directionIsValid(inputDirection, direction)) {
+      direction = inputDirection;
+     
+    }
+
+  }
 
   return {
-    drawSnake:drawSnake,
-    advance: advance
+    drawSnake: drawSnake,
+    advance: advance,
+    setDirection: setDirection
   }
 
 
